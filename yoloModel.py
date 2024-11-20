1e32bd02ae563fa7fed37aec0078b0a7b3df21bb
Sushrut Garikiparthi <s.garikiparthi@berkeley.edu>
2024-11-19 19:09:11 -0800

    updated imgaes

--- yoloModel.py
+++ yoloModel.py
@@ -1,6 +1,16 @@
 from ultralytics import YOLO
-model = YOLO('yolov8n.pt')
+import os
+import random
+model = YOLO('/Users/sushrut.g12/Desktop/PointBlue/Point-Blue-DataGood-Project/runs/detect/train8/weights/best.pt')
 
-model.train(data='data.yaml', epochs=50, imgsz=640, batch=16)
+#model.train(data='data.yaml', epochs=10, imgsz=640, batch=16)
 
-print("trained model")
+image_folder = "/Users/sushrut.g12/Desktop/PointBlue/Point-Blue-DataGood-Project/SkuaDroneImages"
+all_images = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
+print(len(all_images))
+selected_images = random.sample(all_images, 700)
+
+results = model.predict(source= selected_images)
+
+# for result in results:
+#     result.save()  # Saves each result in a `runs/predict` subfolder by default
