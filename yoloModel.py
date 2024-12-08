3b2d871c25edc0ca70fd23cb762c7379eb7b574d
Sushrut Garikiparthi <s.garikiparthi@berkeley.edu>
2024-12-08 14:14:25 -0800

    changes

--- yoloModel.py
+++ yoloModel.py
@@ -1,16 +1,23 @@
 from ultralytics import YOLO
 import os
 import random
-model = YOLO('/Users/sushrut.g12/Desktop/PointBlue/Point-Blue-DataGood-Project/runs/detect/train8/weights/best.pt')
-
-#model.train(data='data.yaml', epochs=10, imgsz=640, batch=16)
-
+model = YOLO("runs/detect/train10/weights/best.pt")
+#model.train(data='data.yaml', epochs=30, imgsz=640, batch=16)
+output_directory = "/Users/sushrut.g12/Desktop/Point-Blue-DataGood-Project/InitialPredictions"
 image_folder = "/Users/sushrut.g12/Desktop/PointBlue/Point-Blue-DataGood-Project/SkuaDroneImages"
 all_images = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
 print(len(all_images))
-selected_images = random.sample(all_images, 700)
+#selected_images = random.sample(all_images, 700)
 
-results = model.predict(source= selected_images)
+#results = model.predict(source= all_images, save=True, save_dir=output_directory)
+results = model.val(data="data.yaml", batch=16,  imgsz=640, save=True) 
+        
+# Extract metrics using the correct methods
+precision = results.box.p       # Mean precision
+recall = results.box.r          # Mean recall
+map50 = results.box.map50        # Mean AP at IoU=0.5
+#map50_95 = results.box.map()       # Mean AP at IoU=0.5:0.95
 
-# for result in results:
-#     result.save()  # Saves each result in a `runs/predict` subfolder by default
+print("Precision", precision)
+print("Recall", recall)
+print("MAP", map50)
