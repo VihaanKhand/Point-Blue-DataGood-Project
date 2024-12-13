7b588b03209595b69b47515d1641f2ded4721119
Sushrut Garikiparthi <s.garikiparthi@berkeley.edu>
2024-12-12 20:30:00 -0800

    done

--- yoloModel.py
+++ yoloModel.py
@@ -6,18 +6,6 @@ import random
 # Load the model
 model = YOLO("runs/detect/train3/weights/best.pt")
 
-# Train with optimized parameters
-# model.train(
-#     data='data.yaml',
-#     epochs=40,              # Adjust based on validation performance
-#     imgsz=640,               # Image size
-#     batch=16,                # Experiment with this value
-#     lr0=0.001,               # Initial learning rate
-#     lrf=0.01,                # Final learning rate (optional)
-#     momentum=0.937,          # Momentum for SGD
-#     weight_decay=0.0005,     # Regularization term
-#     save_period=10           # Save checkpoint every 10 epochs
-# )
 
 # Train with optimized parameters
 model.train(
